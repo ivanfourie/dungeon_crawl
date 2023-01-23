dc83550c018eac5effe59759417bd0d9e179ae5f
Ivan Fourie <ivan.fourie@impact.com>
2023-01-23 19:19:59 +0200

    Implement rooms and corridors

--- src/map.rs
+++ src/map.rs
@@ -9,7 +9,7 @@ pub enum TileType {
 }
 
 pub struct Map {
-    tiles: Vec<TileType>,
+    pub tiles: Vec<TileType>,
 }
 
 
@@ -31,10 +31,10 @@ impl Map {
                 let idx = map_idx(x, y);
                 match self.tiles[idx] {
                     TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('░'));
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
                     }
                     TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('█'));
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('░'));
                     }
                 }
             }
